6b40d08cbd3403fb176fe0792114ff9e2571f258
Jake Ketchum <ketchumjake@gmail.com>
2020-04-30 20:49:56 -0700

    added hello world

--- mining.lua
+++ mining.lua
@@ -1,2 +1,10 @@
-print("This is a Test!")
-print("Hello World!")
+
+local component = require("component")
+local sides = require("sides")
+
+--[
+  A dead simple function that says hello
+--]
+hello_world()
+  print("Hello World!")
+end
