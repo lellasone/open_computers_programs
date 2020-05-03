895a05bd202ce8d9328d4d754aa551da4140b449
Jake Ketchum <ketchumjake@gmail.com>
2020-05-03 09:22:33 -0700

    fixed a raft of minor issues

--- mining.lua
+++ mining.lua
@@ -3,7 +3,7 @@ local component = require("component")
 local sides = require("sides")
 local inventory = component.inventory_controller
 local r = require("robot")
-local g = require("generator")
+local g = component.generator
 
 --[[
   A dead simple function that says hello
@@ -16,11 +16,11 @@ end
 --[[ 
   Locates the specified item in inentory and selects that slot.
   if the slot cannot be found then no change is made. 
-  returns: slot number if found, 0 if not found. 
+  returns: slot number if found, nil if not found. 
 --]]
 function set_item_self(item)
   local temp = find_item_self(item)
-  if temp then 
+  if temp ~= nil then 
     r.select(temp)
     return(temp)
   else 
@@ -35,11 +35,11 @@ end
 --[[
   Find the first instance of the specified item in this device's general 
   inventory. This function does not set a new selected slot. 
-  returns: number of first slot containing item, 0 if none found. 
+  returns: number of first slot containing item, nil if none found. 
 ]]--
 function find_item_self(item)
   for i = 1, r.inventorySize() do
-    temp = inventory.getStackInSlot(side.back, i) --lets see what we have found.
+    temp = inventory.getStackInIntenralSlot(i) --lets see what we have found.
     if(temp ~= nil and temp.name == item) then
       print("found")
       return(i)
@@ -51,7 +51,8 @@ end
 
 --[[
   This function tops up the generator with coal. It is safe to call if the
-  generator is already full, and should be called often as a result. 
+  generator is already full, and should be called often as a result. This
+  function does not change the curser position.
   
   Note: This is not designed to work in a mixed-fuel environment. Only coal 
   will be added. 
@@ -59,17 +60,22 @@ end
   returns: true if generator full or filled, false if no coal is found.
 ]]--
 function add_coal()
-  if count() < 64 then
+  local old_slot = r.select()
+  if g.count() < 64 then
     if set_item_self("minecraft:coal") then
       g.insert(64) --add fuel to the generator
+      r.select(old_slot)
       return('true')
     else
+      r.select(old_slot)
       error("no coal found")
       return(false)
     end
   end
+  r.select(old_slot)
   return(true) --generator already full
+end
 
 print(inventory.getInventorySize(sides.front))
 find_item_self("minecraft:dirt")
-print(add_coal)
+print(add_coal())
