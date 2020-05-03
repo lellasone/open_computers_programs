50ae9d46eeee9a3c8fa8435f71388f897abf2143
Jake Ketchum <ketchumjake@gmail.com>
2020-05-03 01:37:46 -0700

    added tracked turns

--- move.lua
+++ move.lua
@@ -8,6 +8,58 @@ local x = 0 --x axis position relative to reference
 local y = 0 --vertical position relative to reference
 local z = 0 --z position relative to referene
 local h = s.front --heading relative to start
+
+
+--[[
+  This function is the tracked equivilent of turnLeft(). It will turn the robot 
+  left and then update the h value appropriatly. It shold be used in place of 
+  turnLeft(). 
+]]--
+function tracked_left(dir)
+  r.turnLeft()
+  if h = s.left then
+    h = s.back
+  elseif h == s.right then
+    h = s.front
+  elseif h == s.front then
+    h = s.left
+  elseif h == s.back then
+    h = s.right
+  else
+    error ("invalid h")
+  end
+end
+
+
+
+--[[
+  This function is the tracked equivilent of turnRight(). It will turn the robot 
+  right and then update the h value appropriatly. It shold be used in place of 
+  turnLeft(). 
+]]--
+function tracked_right()
+  r.turnRight()
+  if h = s.left then
+    h = s.front
+  elseif h == s.right then
+    h = s.back
+  elseif h == s.front then
+    h = s.right
+  elseif h == s.back then
+    h = s.left
+  else
+    error ("invalid h")
+  end
+end
+
+
+--[[
+  This function moves the robot forward relative to it's current heading and 
+  updates the tracking coordinates appropriatly to match. If the robot does 
+  not move then the coordinates will not be updated. This should be moved 
+  in place of forward() in pretty much all 
+  cases. 
+]]--
 function tracked_move()
   if (r.forward())
     if h = s.front then
@@ -25,4 +77,12 @@ function tracked_move()
   end
   return(nil)
   
+end
+
+--[[
+  This function is used for reporting errors as the occur. 
+  for now it is a place holder which prints the error. 
+  ]]--
+function error(msg)
+  print(msg)
 end
