3a64f3436b8a2cdd794f664119405bc5a8c6e0b1
Jake Ketchum <ketchumjake@gmail.com>
2020-05-07 17:02:44 -0700

    fixed for loop issues and changed print

--- comb.lua
+++ comb.lua
@@ -190,7 +190,7 @@ end
   to be in the way. It uses tracked movement. 
  ]]--
  function break_line(length)
-   for i = 0, i < length, + 1 do
+   for i = 0, i < length, 1 do
      break_move()
    end
  end
@@ -333,8 +333,9 @@ end
   This function outputs the inventory to the console for debugging purposes. 
 --]]
 function print_inventory()
-  for i = 1,  i <= r.inventorySize(), + 1 do
-    print(inventory.getStackInInternalSlot(i))
+  for i = 1,  i <= r.inventorySize(),  1 do
+    local temp = inventory.getStackInInternalSlot(i)
+    if temp.name ~= nil then print(temp.name) end
   end
 end
 
