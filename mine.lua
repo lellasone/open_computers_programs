ccb810b39e243e9f2f9f44f2224e4720312cc3a5
Jake Ketchum <ketchumjake@gmail.com>
2020-05-14 13:19:40 -0700

    removed swirl pattern

--- mine.lua
+++ mine.lua
@@ -359,19 +359,13 @@ function mine_column(timeout)
 	-- lets place our scaffold. 
 	set_item_self(SCAFFOLD_MATERIAL)
 	-- make a cool swirl pattern
-        if z%1 == 0 then r.place(s.front) end
-	if z%2 == 0 then r.place(s.right) end
-  	if z%3 == 0 then r.place(s.back) end
-   	if z%4 == 0 then r.place(s.left) end
+        r.place()
 	if(r.swingDown() == false) then break end
     end
     while z < z_start do
         set_item_self(SCAFFOLD_MATERIAL)
 	-- make a cool swirl pattern
-        if z%1 == 0 then r.place(s.front) end
-	if z%2 == 0 then r.place(s.right) end
-  	if z%3 == 0 then r.place(s.back) end
-   	if z%4 == 0 then r.place(s.left) end
+        r.place()
         break_up() 
     end
 end
