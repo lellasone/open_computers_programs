484106020b2e0a77c52120b3249f6d8d17a84c66
Jake Ketchum <ketchumjake@gmail.com>
2022-02-03 17:10:04 -0600

    fixed incorrect name

--- autorun.lua
+++ autorun.lua
@@ -9,7 +9,7 @@ startup_action = function()
     event.ignore("term_available", startup_action)
     term.clear()
     print("starting mining program, press control-c now to exit")
-    os.wait(5)
+    os.sleep(5)
     print("starting mine.lua")
     dofile("mine.lua")
 end
