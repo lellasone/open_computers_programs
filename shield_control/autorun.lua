03c6ca16b133bdb48265c474e52ce31bbc2737ab
Jake Ketchum <ketchumjake@gmail.com>
2022-02-06 10:31:36 -0600

    Final form of both programs

--- shield_control/autorun.lua
+++ shield_control/autorun.lua
@@ -1,15 +1,29 @@
 
---[[
+--[[ This is a lua program for controlling a warpdrive force field generator 
+based on open computers network messages. This program should be placed on a 
+floppy in a computer connected to the projector, and that computer should be
+connected by network (wired or wireless) to a computer running
+"password.lua"
+
+This program can either be used to create an external access terminal for a
+secured facility (wireless cards), or to create a command terminal within a 
+ship/base (wired card). 
+
+The computer should be Tier 3, with T2 ran T3 CPU, T1 hard drive, and a screen
+with keyboard. OpenOS should be loaded
 
 Commands:
     "password" - Lower shield for 60s.
     "password:on" - Activate shield. 
     "password:off" - Lower shield.
+    "password:ping" - Do nothing, but return a valid response. (good for 
+        debugging)
 --]]
 port = 666
 password = "gdbg"
 off = password..":off"
 on = password..":on"
+ping = password..":ping"
 
 local component = require("component")
 local event = require("event")
@@ -27,7 +41,7 @@ startup_action = function()
     -- Set up the auto-wake. 
     print("setting startup message")
     modem.setWakeMessage("wake")
-    modem.isOpen(666)
+    modem.open(port)
   
     while true
     do 
@@ -36,20 +50,23 @@ startup_action = function()
         if message == on then
             print("got shield enable command")
             shield.enable(1)
-            modem.broadcast(666,"good")
+            modem.broadcast(port,"good")
         elseif message == off then
             print("got shield disable command")
             shield.disable(0)
-            modem.broadcast(666,"good") 
+            modem.broadcast(port,"good") 
         elseif message == password then
             print("got shield flicker command, lowering for 60s")
             shield.enable(0)
-            modem.broadcast(666,"good")
+            modem.broadcast(port,"good")
             os.sleep(60)
             shield.enable(1)
+        elif message = ping then
+            print("got valid password ping. Taking no actions."
+            modem.broadcast(port,"good")
         elseif message ~= nil then
             print("got invalid message")
-            modem.broadcast(666,"bad")
+            modem.broadcast(port,"bad")
         end
     end
     print("program has ended, shutting down")
--- shield_control/password.lua
+++ shield_control/password.lua
@@ -1,5 +1,8 @@
--- This program broadcasts a phrase to all nearby computers. Good for sending
--- pass phrases, although note that it does so in the clear. 
+--[[ This program broadcasts commands to a remote computer. It was initially
+intended as an access terminal for a shielded facility and works well in that
+capacity (although note that passphrases are sent in the clear). 
+
+The computer should be T3 with T2 ram, cpu, T1 hard drive, and openos loaded.  
 
 port = 666
 local component = require("component")
@@ -7,9 +10,9 @@ local m = component.modem
 local computer = require("computer")
 local event = require("event")
 
-print("Booting up remote computer, please wait 4s")
+print("Booting up remote computer, please wait 0.5s")
 m.broadcast(port, "wake")
-os.sleep(4)
+os.sleep(.5)
 
 
 print("Please enter passcode and (optional) command. Then press enter to send")
