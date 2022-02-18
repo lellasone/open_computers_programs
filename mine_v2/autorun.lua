4d5bdd8e367114c00cdc3ff22b25ec61a122a4f1
Jake <ketchumjake@gmail.com>
2022-02-18 10:21:38 -0600

    added support autorun on floppy

--- mine_v2/autorun.lua
+++ mine_v2/autorun.lua
@@ -4,11 +4,21 @@
 local event = require("event")
 local term = require("term")
 
+drive_label =  nil
+
 local startup_action
+local mnt_add = "default_something_broke"
 startup_action = function()
     event.ignore("term_available", startup_action)
     term.clear()
+    print("finding floppy drive location")
+    for address in component.list('filesystem')do
+        if component.proxy(address).getLabel() == drive_label then
+            mnt_add = string.sub(address,0,3)
+        end
+    end
+    print("got address: ",mnt_add)
     print("starting mine.lua")
-    os.execute("cd floppy && ./mine.lua")
+    os.execute(string.format("cd %s && ./mine.lua")
 end
 event.listen("term_available", startup_action)
